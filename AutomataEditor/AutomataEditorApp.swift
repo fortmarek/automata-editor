d527d0712f2f01cdb293cc9f8713c24b0c44ff2e
fortmarek <marekfort@me.com>
2021-02-17 10:42:09 +0100

    Initial commit.

---
+++ AutomataEditor/AutomataEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomataEditorApp.swift
+//  AutomataEditor
+//
+//  Created by Marek Fořt on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AutomataEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
