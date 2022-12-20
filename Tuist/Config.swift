d19c711ff81442ad5c0cec379f94c19063fd1823
fortmarek <marekfort@me.com>
2022-12-20 22:26:08 +0100

    Add cloud analytics

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,5 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-    cloud: .cloud(projectId: "marekfort/automata-editor", url: "https://cloud.tuist.io", options: [.optional])
+    cloud: .cloud(projectId: "marekfort/automata-editor", url: "https://cloud.tuist.io", options: [.optional, .analytics])
 )
